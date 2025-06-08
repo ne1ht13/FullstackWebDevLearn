32d693437d1e2605e44ed87fa3454a70c9f6e245
Thien Truong <locthien428@gmail.com>
2025-06-09 00:07:03 +0700

    Component state, event handlers
    Learning and testing Component state, event handlers

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,33 +1,77 @@
-import './App.css'
-// Part1: FUNDAMENTAL FOR REACT
-const Hello = (props) => {
+import { useState } from 'react'
 
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+//Passing state - to child components
+const Display = ({ counter }) => <div>{counter}</div>
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
 
-  const name = 'Peter'
-  const age = 10
+  //Page re-rendering and Stateful component
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
+
+
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
+
+  // console.log('rendering...', counter)
+
+  //Event Handler
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      {/* <Hello name='Maya' age={26 + 10} /> */}
+      <Display counter={counter}/>
+     <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
-
 }
-
-//PART2: JAVA
 
 export default App
