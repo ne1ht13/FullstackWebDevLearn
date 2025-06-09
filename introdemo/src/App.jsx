be6d5fefcbe4a64316736d95e8012312f1f8c0e5
Thien Truong <locthien428@gmail.com>
2025-06-09 23:15:25 +0700

    A more complex state, debugging React apps

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -17,8 +17,26 @@ import { useState } from 'react'
 //Passing state - to child components
 const Display = ({ counter }) => <div>{counter}</div>
 
-
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+//Conditional rendering
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+
 
 const App = () => {
 
@@ -37,6 +55,9 @@ const App = () => {
 
   // console.log('rendering...', counter)
 
+  
+  
+
   //Event Handler
   const increaseByOne = () => {
     console.log('increasing, value before', counter)
@@ -53,6 +74,36 @@ const App = () => {
   }
 
 
+  //Complex state
+//  const [clicks, setClicks] = useState({
+//     left: 0, right: 0
+//   })
+
+  // const handleLeftClick = () =>
+  // setClicks({ ...clicks, left: clicks.left + 1 })
+
+  // const handleRightClick = () =>
+  // setClicks({ ...clicks, right: clicks.right + 1 })
+
+  //Handling Array and asynchronous
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right +1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
 
   return (
     <div>
@@ -69,7 +120,28 @@ const App = () => {
       <Button
         onClick={decreaseByOne}
         text='minus'
-      />           
+      />
+      {/* Complex State 
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div> */}
+
+      {/* Handling arrays and asynchronous */}
+      <div>
+        {left}
+        {/* <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button> */}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <p>{allClicks.join(' ')}</p>
+        <p>total {total}</p>
+        {/* Conditional rendering */}
+        <History allClicks={allClicks} /> 
+      </div>
     </div>
   )
 }
