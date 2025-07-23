5100842d2fa9adcb3f8cb3b98366831589dc8b61
Loc Thien <110154036+ne1ht13@users.noreply.github.com>
2025-07-23 11:25:38 +0700

    Advanced state management
    Communicating with server in a Redux application

--- flux_architecture/src/App.jsx
+++ flux_architecture/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
 import NewNote from "./components/NewNote"
 import Notes from "./components/Notes"
 import VisibilityFilter from './components/VisibilityFilter'
+import noteService from './services/notes'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() =>{
+   dispatch(initializeNotes()) 
+  },[])
+
   return (
     <div>
       <NewNote />
